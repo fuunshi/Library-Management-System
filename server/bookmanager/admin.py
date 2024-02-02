60e01b75c0da25df13f606555d47dba57763d720
fuunshi <pranilshrestha2016@gmail.com>
2024-02-02 08:29:12 +0545

    Models

--- server/bookmanager/admin.py
+++ server/bookmanager/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.register.site(User)
+admin.register.site(Book)
+admin.register.site(BookDetails)
+admin.register.site(BorrowedBooks)
--- server/bookmanager/models.py
+++ server/bookmanager/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class User(models.Model):
+    UserID = models.CharField(max_length=50, unique=True)
+    name = models.CharField(max_length=100)
+    email = models.CharField(max_length=100, unique=True)
+    membershipDate = models.DateField(default=timezone.now())
+
+    def __str__(self):
+        return self.name
+    
+class Book(models.Model):
+    bookID = models.CharField(max_length=50, unique=True)
+    title = models.CharField(max_length=100)
+    ISBN = models.IntegerField(max_length=13, unique=True)
+    publishedDate = models.DateField()
+    
+    def __str__(self):
+        return self.title
+
+class BookDetails(models.Model):
+    detailsID = models.CharField(max_length=50, unique=True)
+    bookID = models.ForeignKey(Book, verbose_name=("bookID"), on_delete=models.CASCADE)
+    numberOfPages = models.IntegerField()
+    publisher = models.CharField(max_length=50)
+    language = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.detailsID
+
+class BorrowedBooks(models.Model):
+    userID = models.ForeignKey(User, verbose_name=("userID"), on_delete=models.CASCADE)
+    bookID = models.ForeignKey(Book, verbose_name=("bookID"), on_delete=models.CASCADE)
+    borrowDate = models.DateField()
+    returnDate = models.DateField()
+
+    def __str__(self):
+        return f'{self.bookID} by {self.userID}'
